57f2f2733527aae65282d51e208244eecb2ca1d1
Wyatt Kraai <75582820+iMacWeirdo@users.noreply.github.com>
2025-09-11 13:23:55 -0500

    Create filler.f

---
+++ lawn/filler.f
@@ -0,0 +1,20 @@
+int.create filler.f | filler.f = "filler" && .f = end & .gt = gentext + "ver.5.1.00"
+def bvar 
+
+[filler.f]
+  {gt.html5}
+   <!DOCTYPE html>
+   <html>
+     <head>
+       <meta charset="utf-8">
+     </head>
+     <body>
+       <p><a href="/filler.f">filler.f</a></p>
+     </body>
+   </html>
+  {gt.stend}
+[.f]
+
+[filler]
+doc.descrip == Filler file, based on gentext 5.
+  
